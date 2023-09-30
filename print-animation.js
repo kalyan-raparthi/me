4dd393c6aabd5de426023ce1f68d3f9f5baeaec9
qb <qb_kalyan>
2023-09-30 11:16:27 +0530

    phase1

--- print-animation.js
+++ print-animation.js
@@ -1,18 +1,38 @@
 const a  = document.getElementById("riddle")
 let arr = "Enter your password."
 
-async function print() {
+async function EXE(ar) {
     let exe = document.createTextNode("EXIXE: ")
-    a.appendChild(exe)
+    document.body.appendChild(exe)
     for (let i = 0; i < arr.length; i++) {
         await sleep(100);
-        var txt = document.createTextNode(arr.charAt(i))
-        a.appendChild(txt)
+        var txt = document.createTextNode(ar.charAt(i))
+        document.body.appendChild(txt)
     }
-
+    let i  = document.createElement("br")
+    document.body.appendChild(i)
 }
+
 async function sleep(num) {
     return new Promise((resolve) => setTimeout(resolve, num))
 }
 
-print()
+async function POST() {
+    var form = document.createElement("form")
+    form.setAttribute("method", "POST");
+    var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("name", "code");
+    input.setAttribute("autofocus", "on")
+    form.appendChild(input)
+    document.body.appendChild(form)
+}
+
+async function main() {
+    await EXE(arr)
+    await POST()  
+}
+
+
+main()
+
