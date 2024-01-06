833b4f2934009afa2aad384aa90e7094fdf10be1
qb <kalyan.raparthi@hotmail.com>
2024-01-06 10:06:23 +0530

    all

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var val = true;
 function bt() {
     let img0 = document.getElementById('img-0');
     if(val) {
-        img0.innerHTML= '<div id="img-0"><img src="/media/qb_img/eixie.png" alt="qb0.jpg"><div class="cc-div"><p class="caption">e i x i e </p><p class="timeposted">[08:10:10:19:2023]</p></div></div>'
+        img0.innerHTML= '<div id="img-0"><img src="/media/qb_img/eixie.png" alt="qb0.jpg"><div class="pdiv"><p class="caption">e i x i e </p><p class="timeposted">[08:10:10:19:2023]</p></div></div>'
         val = false;
     }
     else if(!val){
