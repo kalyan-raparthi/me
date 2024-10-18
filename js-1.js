7fb987a2304698e1b4d8b7e1c9567cbce4e923c9
qb <kalyan.raparthi@hotmail.com>
2024-10-19 00:48:34 +0530

    full screen mode test - button v1

--- js-1.js
+++ js-1.js
@@ -1,19 +1,26 @@
-// function toggleFullScreen() {
-//     const element = document.documentElement;
-//     const button = document.getElementById('enter-button');
-  
-//     if (element.requestFullscreen) {
-//       element.requestFullscreen();
-//       button.parentNode.removeChild(button);
-//     } else if (element.mozRequestFullScreen) { /* Firefox */
-//         element.mozRequestFullScreen();
-//     } else if (element.webkitRequestFullscreen) { /* Chrome, Safari, Opera */
-//         element.webkitRequestFullscreen();
-//     } else if (element.msRequestFullscreen) { /* IE/Edge */
-//         element.msRequestFullscreen();
-//     }
-//   }
+function addParagraph() {
+    const contentDiv = document.getElementById('content');
 
+    const heading = document.createElement('h1');
+    heading.textContent = '[qb]';
+    
+    
+    const paragraph = document.createElement('p');
+    paragraph.textContent = '> ';
+    
+    
+    const button = document.createElement('button');
+    button.textContent = 'SHOW_PROJECTS';
+    button.addEventListener('click',  
+        () => {
+            
+        });
+        
+    contentDiv.appendChild(heading);
+    contentDiv.appendChild(paragraph);
+    contentDiv.appendChild(button);
+  
+  }
 
 async function toggleFullScreen() {
     const element = document.documentElement;
@@ -31,6 +38,7 @@ async function toggleFullScreen() {
   
       const button = document.getElementById('enter-button');
       button.parentNode.removeChild(button);
+      addParagraph()
     } catch (error) {
       console.error('Error entering full-screen mode:', error);
     }
