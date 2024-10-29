282300e5ae9280dd9c884f610f273602cd945399
qb <83409253+qb-bit@users.noreply.github.com>
2024-10-29 06:15:52 +0530

    Update js-1.js

--- js-1.js
+++ js-1.js
@@ -11,14 +11,20 @@ function addParagraph() {
 
     const heading = document.createElement('h1');
     heading.textContent = '[qb]';
-    
-    const anchor = document.createElement('a');
-    anchor.href = 'https://www.google.com';
-    anchor.textContent = 'SHOW_PROJECTS';
-          const container = document.getElementById('content'); // Replace with your container element ID
+    // SHOW_PROJECTS
+    const anchor_1 = document.createElement('a');
+    anchor_1.href = 'https://www.google.com';
+    anchor_1.textContent = 'SHOW_PROJECTS';
+    const container = document.getElementById('content'); 
+    // ADVANCED_C_REF
+    const anchor_2 = document.createElement('a');
+    anchor_2.href = 'C_REF.html';
+    anchor_2.textContent = 'ADVANCED_C_REF';
+    const container = document.getElementById('content'); 
     
     contentDiv.appendChild(heading);  
-    contentDiv.appendChild(anchor);
+    contentDiv.appendChild(anchor_1);
+    contentDiv.appendChild(anchor_2);
 }
 
 async function toggleFullScreen() {
